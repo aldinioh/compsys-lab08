4d46019173eabe0396c78b93ccf86b920704390b
aldinioh <aldin0505@gmail.com>
2025-05-10 22:41:36 +1000

    8.2.2(d)

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # compsys-lab08
+
+## 8.2.2:
+
+### 8.2.2(a) 
+
+The way I would write it would be as follows:
+
+`x >= 0 && x <= 4`
+
+<br>
+
+or specifically:
+
+```rs
+if x < 1 {
+    // Do stuff
+} else if x > 3 {
+    // Do more stuff
+} else {
+    continue; // assuming we are already in some type of loop
+}
+```
+
+### 8.2.2(b) 
+
+for the first conditional statement (x < 1) we could use BLT and for x > 3 we can use BGT. 
+
+### 8.8.2(c)
+
+In the case x > 0, N would be set to 1, as the main idea of x > 0 conditional statement is to check if x is GREATER than 0. 
+
+<br>
+
+For x < 4, it would be overflow as it has gone past the desired range and therefore cause an overflow in BITSS
+
+
+
--- lab08.asm
+++ lab08.asm
@@ -1,5 +1,6 @@
 ; R0 keeps track of  current amount of matchsticks present
 ; R1 is the register where messages are outputted from
+; R2 is the register of the current user's inputted number
 
 ; Store number 15 to R0 (this is the default starting value of R0,)
 mov R0, #15
@@ -17,8 +18,30 @@ str R1, .WriteString
 mov R1, #removeMessage
 str R1, .WriteString
 
+RangeLoop:
+
 ; Store the input that is recived from user
 ldr R2, .InputNum
+
+; We need to check if the input (R2) is less than 1, if it is go to next conditional and perform another check.
+cmp R2, #1
+blt outOfRange
+
+; Check if input (R2) is greater than 3 
+cmp R2, #3
+bgt outOfRange
+
+; If both compares are true than this will be executed
+B InRange
+
+outOfRange:
+mov R1, #outOfRangeMessage
+str R1, .WriteString
+B RangeLoop ; Go back because it was not in range
+
+
+InRange:
+; Subtract R0 and R2 and store new value in R0
 sub R0, R0, R2
 
 B Loop
@@ -27,4 +50,5 @@ HALT
 
 ; Messages that will be outputted
 remainingMessage: .ASCIZ "remaining\n"
-removeMessage: .ASCIZ "How many do you want to remove (1-3)?"
+removeMessage: .ASCIZ "How many do you want to remove (1-3)?\n"
+outOfRangeMessage: .ASCIZ "\nInput out of range! Please enter (1-3).\n"
